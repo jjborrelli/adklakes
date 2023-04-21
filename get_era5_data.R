c8c23f86bf1a40ee3aecffb7611d6d1faf195219
jjborrelli <borrejj@gmail.com>
2023-04-21 10:15:20 -0400

    compile and save era5 data

--- get_era5_data.R
+++ get_era5_data.R
@@ -25,9 +25,10 @@ metvars <- c("2m_temperature", "2m_dewpoint_temperature", "10m_u_component_of_wi
 request <- list(
   dataset_short_name = "reanalysis-era5-single-levels",
   product_type   = "reanalysis",
-  format = "2m_temperature",
-  variable = metvars,
-  year = as.character(1990),
+  # format = "2m_temperature",
+  format = "large_scale_rain_rate",
+  variable = "large_scale_rain_rate",
+  year = as.character(1993),
   month = stringr::str_pad(1:12,2,"left","0"),
   day = stringr::str_pad(1:31,2,"left","0"),
   time = c("00:00", "01:00", "02:00", "03:00", "04:00", "05:00", 
@@ -38,7 +39,7 @@ request <- list(
   # area = c(45.01157, -79.76718, 40.4852, -71.87756),
   area = c(44.87791, -75.31968, 43.05235, -73.29350), # adk park
   # area = c(43.83786, -73.71255, 43.41753, -73.42483),
-  target = "era5_airtemp2m_1990.nc"
+  target = "era5/era5_large_scale_rain_rate_1993.nc"
 )
 
 t1 <- Sys.time()
@@ -51,16 +52,19 @@ t2 <- Sys.time()
 t2-t1
 
 
-mlst <- c("2m_temperature", "2m_dewpoint_temperature", 
-          "10m_u_component_of_wind",
-          "10m_v_component_of_wind", "large_scale_rain_rate", 
-          "surface_pressure",
-          "large_scale_snowfall_rate_water_equivalent",
-          "mean_surface_downward_short_wave_radiation_flux",
-          "mean_surface_downward_long_wave_radiation_flux")
-for(x in mlst[-6]){
+mlst <- c(#"2m_temperature", 
+          #"2m_dewpoint_temperature", 
+          #"10m_u_component_of_wind",
+          #"10m_v_component_of_wind", 
+          "large_scale_rain_rate"#, 
+          #"surface_pressure",
+          #"large_scale_snowfall_rate_water_equivalent",
+          #"mean_surface_downward_short_wave_radiation_flux",
+          #"mean_surface_downward_long_wave_radiation_flux"
+          )
+for(x in mlst){
   mvar <- x
-  for(i in 1992:2012){
+  for(i in 1994:2012){
     
     request <- list(
       dataset_short_name = "reanalysis-era5-single-levels",
@@ -105,10 +109,10 @@ library(ggplot2)
 library(lubridate)
 library(dplyr)
 
-files <- list.files("era5", pattern = "2m_dewpoint")
+files <- list.files("data/era5", pattern = "2m_dewpoint")
 dpdata <- list()
 for(i in seq_along((files))){
-  nc <- nc_open(paste0("era5/", files[i]))
+  nc <- nc_open(paste0("data/era5/", files[i]))
   data <- ncvar_get(nc,"d2m")
   
   lat <- ncvar_get(nc,'latitude')
@@ -116,18 +120,19 @@ for(i in seq_along((files))){
   times <- lubridate::hours(ncvar_get(nc, 'time')) + ymd_hms("1900-01-01 00:00:00")
   
   dpdata[[i]] <- reshape2::melt(data) %>% 
-    mutate(lat = lat[Var1], lon = lon[Var2], datetime = times[Var3]) %>% 
-    group_by(datetime) %>% summarize(dewpt_2m = mean(value))
+    mutate(lat = lat[Var2], lon = lon[Var1], datetime = times[Var3]) %>% 
+    mutate(dewpt_2m = value) %>% 
+    select(lat, lon, datetime, dewpt_2m)
 }
+dpdata <- data.table::rbindlist(dpdata)
 
 
 
 
-
-files <- list.files("era5", pattern = "2m_temperature")
+files <- list.files("data/era5", pattern = "2m_temperature")
 tmpdata <- list()
 for(i in seq_along((files))){
-  nc <- nc_open(paste0("era5/", files[i]))
+  nc <- nc_open(paste0("data/era5/", files[i]))
   data <- ncvar_get(nc,"t2m")
   
   lat <- ncvar_get(nc,'latitude')
@@ -135,16 +140,28 @@ for(i in seq_along((files))){
   times <- lubridate::hours(ncvar_get(nc, 'time')) + ymd_hms("1900-01-01 00:00:00")
   
   tmpdata[[i]] <- reshape2::melt(data) %>% 
-    mutate(lat = lat[Var1], lon = lon[Var2], datetime = times[Var3]) %>% 
-    group_by(datetime) %>% summarize(temp_2m = mean(value))
+    mutate(lat = lat[Var2], lon = lon[Var1], datetime = times[Var3]) %>% 
+    mutate(temp_2m = value) %>% 
+    select(lat, lon, datetime, temp_2m)
 }
 
 
+tmpdata <- data.table::rbindlist(tmpdata)
+# 
+# test %>% 
+#   filter(year(datetime) == 2012) %>% 
+#   ggplot() + 
+#   geom_raster(aes(x = lon, y = lat, fill = temp_2m-273.15)) +
+#   geom_sf(data = st_transform(adkalt, "WGS84"), fill = NA, color = "black", linewidth = 1.2) + 
+#   scale_fill_viridis_c(option = "B") + 
+#   labs(title = "{frame_time}") + 
+#   transition_time(datetime)
 
-files <- list.files("era5", pattern = "10m_u_component")
+
+files <- list.files("data/era5", pattern = "10m_u_component")
 wndudata <- list()
 for(i in seq_along((files))){
-  nc <- nc_open(paste0("era5/", files[i]))
+  nc <- nc_open(paste0("data/era5/", files[i]))
   data <- ncvar_get(nc,"u10")
   
   lat <- ncvar_get(nc,'latitude')
@@ -152,16 +169,17 @@ for(i in seq_along((files))){
   times <- lubridate::hours(ncvar_get(nc, 'time')) + ymd_hms("1900-01-01 00:00:00")
   
   wndudata[[i]] <- reshape2::melt(data) %>% 
-    mutate(lat = lat[Var1], lon = lon[Var2], datetime = times[Var3]) %>% 
-    group_by(datetime) %>% summarize(wind_u = mean(value))
+    mutate(lat = lat[Var2], lon = lon[Var1], datetime = times[Var3]) %>%  
+    mutate(wind_u = value) %>% 
+    select(lat, lon, datetime, wind_u)
 }
+wndudata <- data.table::rbindlist(wndudata)
 
 
-
-files <- list.files("era5", pattern = "10m_v_component")
+files <- list.files("data/era5", pattern = "10m_v_component")
 wndvdata <- list()
 for(i in seq_along((files))){
-  nc <- nc_open(paste0("era5/", files[i]))
+  nc <- nc_open(paste0("data/era5/", files[i]))
   data <- ncvar_get(nc,"v10")
   
   lat <- ncvar_get(nc,'latitude')
@@ -169,18 +187,20 @@ for(i in seq_along((files))){
   times <- lubridate::hours(ncvar_get(nc, 'time')) + ymd_hms("1900-01-01 00:00:00")
   
   wndvdata[[i]] <- reshape2::melt(data) %>% 
-    mutate(lat = lat[Var1], lon = lon[Var2], datetime = times[Var3]) %>% 
-    group_by(datetime) %>% summarize(wind_v = mean(value))
+    mutate(lat = lat[Var2], lon = lon[Var1], datetime = times[Var3]) %>%  
+    mutate(wind_v = value) %>% 
+    select(lat, lon, datetime, wind_v)
 }
+wndvdata <- data.table::rbindlist(wndvdata)
 
 
 
 
 
-files <- list.files("era5", pattern = "rain_rate")
+files <- list.files("data/era5", pattern = "rain_rate")
 raindata <- list()
 for(i in seq_along((files))){
-  nc <- nc_open(paste0("era5/", files[i]))
+  nc <- nc_open(paste0("data/era5/", files[i]))
   data <- ncvar_get(nc,"lsrr")
   
   lat <- ncvar_get(nc,'latitude')
@@ -188,16 +208,17 @@ for(i in seq_along((files))){
   times <- lubridate::hours(ncvar_get(nc, 'time')) + ymd_hms("1900-01-01 00:00:00")
   
   raindata[[i]] <- reshape2::melt(data) %>% 
-    mutate(lat = lat[Var1], lon = lon[Var2], datetime = times[Var3]) %>% 
-    group_by(datetime) %>% summarize(precip = mean(value))
+    mutate(lat = lat[Var2], lon = lon[Var1], datetime = times[Var3]) %>%  
+    mutate(precip = value) %>% 
+    select(lat, lon, datetime, precip)
 }
+raindata <- data.table::rbindlist(raindata)
 
 
-
-files <- list.files("era5", pattern = "downward_long_wave")
+files <- list.files("data/era5", pattern = "downward_long_wave")
 dwldata <- list()
 for(i in seq_along((files))){
-  nc <- nc_open(paste0("era5/", files[i]))
+  nc <- nc_open(paste0("data/era5/", files[i]))
   data <- ncvar_get(nc,"msdwlwrf")
   
   lat <- ncvar_get(nc,'latitude')
@@ -205,16 +226,17 @@ for(i in seq_along((files))){
   times <- lubridate::hours(ncvar_get(nc, 'time')) + ymd_hms("1900-01-01 00:00:00")
   
   dwldata[[i]] <- reshape2::melt(data) %>% 
-    mutate(lat = lat[Var1], lon = lon[Var2], datetime = times[Var3]) %>% 
-    group_by(datetime) %>% summarize(downward_longwave = mean(value))
+    mutate(lat = lat[Var2], lon = lon[Var1], datetime = times[Var3]) %>%  
+    mutate(downward_longwave = value) %>% 
+    select(lat, lon, datetime, downward_longwave)
 }
+dwldata <- data.table::rbindlist(dwldata)
 
 
-
-files <- list.files("era5", pattern = "downward_short_wave")
+files <- list.files("data/era5", pattern = "downward_short_wave")
 dwsdata <- list()
 for(i in seq_along((files))){
-  nc <- nc_open(paste0("era5/", files[i]))
+  nc <- nc_open(paste0("data/era5/", files[i]))
   data <- ncvar_get(nc,"msdwswrf")
   
   lat <- ncvar_get(nc,'latitude')
@@ -222,15 +244,16 @@ for(i in seq_along((files))){
   times <- lubridate::hours(ncvar_get(nc, 'time')) + ymd_hms("1900-01-01 00:00:00")
   
   dwsdata[[i]] <- reshape2::melt(data) %>% 
-    mutate(lat = lat[Var1], lon = lon[Var2], datetime = times[Var3]) %>% 
-    group_by(datetime) %>% summarize(downward_shortwave = mean(value))
+    mutate(lat = lat[Var2], lon = lon[Var1], datetime = times[Var3]) %>%   
+    mutate(downward_shortwave = value) %>% 
+    select(lat, lon, datetime, downward_shortwave)
 }
+dwsdata <- data.table::rbindlist(dwsdata)
 
-
-files <- list.files("era5", pattern = "surface_pressure")
+files <- list.files("data/era5", pattern = "surface_pressure")
 spresdata <- list()
 for(i in seq_along((files))){
-  nc <- nc_open(paste0("era5/", files[i]))
+  nc <- nc_open(paste0("data/era5/", files[i]))
   data <- ncvar_get(nc,"sp")
   
   lat <- ncvar_get(nc,'latitude')
@@ -238,43 +261,48 @@ for(i in seq_along((files))){
   times <- lubridate::hours(ncvar_get(nc, 'time')) + ymd_hms("1900-01-01 00:00:00")
   
   spresdata[[i]] <- reshape2::melt(data) %>% 
-    mutate(lat = lat[Var1], lon = lon[Var2], datetime = times[Var3]) %>% 
-    group_by(datetime) %>% summarize(surface_press = mean(value))
+    mutate(lat = lat[Var2], lon = lon[Var1], datetime = times[Var3]) %>%   
+    mutate(surface_press = value) %>% 
+    select(lat, lon, datetime, surface_press)
 }
-
-files <- list.files("era5", pattern = "snowfall")
-snowdata <- list()
-for(i in seq_along((files))){
-  nc <- nc_open(paste0("era5/", files[i]))
-  data <- ncvar_get(nc,"lssfr")
-  
-  lat <- ncvar_get(nc,'latitude')
-  lon <- ncvar_get(nc,'longitude')
-  times <- lubridate::hours(ncvar_get(nc, 'time')) + ymd_hms("1900-01-01 00:00:00")
-  
-  snowdata[[i]] <- reshape2::melt(data) %>% 
-    mutate(lat = lat[Var1], lon = lon[Var2], datetime = times[Var3]) %>% 
-    group_by(datetime) %>% summarize(snow = mean(value))
-}
+spresdata <- data.table::rbindlist(spresdata)
+# 
+# files <- list.files("era5", pattern = "snowfall")
+# snowdata <- list()
+# for(i in seq_along((files))){
+#   nc <- nc_open(paste0("era5/", files[i]))
+#   data <- ncvar_get(nc,"lssfr")
+#   
+#   lat <- ncvar_get(nc,'latitude')
+#   lon <- ncvar_get(nc,'longitude')
+#   times <- lubridate::hours(ncvar_get(nc, 'time')) + ymd_hms("1900-01-01 00:00:00")
+#   
+#   snowdata[[i]] <- reshape2::melt(data) %>% 
+#     mutate(lat = lat[Var1], lon = lon[Var2], datetime = times[Var3]) %>% 
+#     group_by(datetime) %>% summarize(snow = mean(value))
+# }
 
 # relhum from: https://www.omnicalculator.com/physics/relative-humidity
-met <- read.csv("LakeEnsemblR_meteo_standard.csv")
+met <- read.csv("LakeEnsemblR_meteo_adk_sub.csv")
 head(met)
 
-era5met <- do.call(rbind, dwsdata) %>% 
-  left_join(do.call(rbind, dwldata), by = "datetime") %>% 
-  left_join(do.call(rbind, dpdata), by = "datetime") %>% 
-  left_join(do.call(rbind, wndudata), by = "datetime") %>% 
-  left_join(do.call(rbind, wndvdata), by = "datetime") %>% 
-  left_join(do.call(rbind, raindata), by = "datetime") %>% 
-  left_join(do.call(rbind, tmpdata), by = "datetime") %>% 
-  left_join(do.call(rbind, spresdata), by = "datetime") %>% 
-  left_join(do.call(rbind, snowdata), by = "datetime") %>% 
-  mutate(wind_spd = sqrt(wind_v^2 + wind_u^2), 
+era5met <- dwsdata %>%
+  left_join(dwldata, by = c("datetime", "lat", "lon")) %>%
+  left_join(dpdata, by = c("datetime", "lat", "lon")) %>%
+  left_join(wndudata, by = c("datetime", "lat", "lon")) %>%
+  left_join(wndvdata, by = c("datetime", "lat", "lon")) %>%
+  left_join(raindata, by = c("datetime", "lat", "lon")) %>%
+  left_join(tmpdata, by = c("datetime", "lat", "lon")) %>%
+  left_join(spresdata, by = c("datetime", "lat", "lon")) %>%
+  # left_join(do.call(rbind, snowdata), by = "datetime") %>%
+  mutate(wind_spd = sqrt(wind_v^2 + wind_u^2),
          temp_2m = temp_2m - 273.15, dewpt_2m = dewpt_2m - 273.15,
          relhum = 100 * ((exp((17.625 * dewpt_2m)/(243.04+dewpt_2m))/
-                            exp((17.625 * temp_2m)/(243.04+temp_2m))))) %>% 
-  select(datetime, downward_shortwave,downward_longwave, temp_2m, 
-         relhum, wind_spd, precip, snow, surface_press)
+                            exp((17.625 * temp_2m)/(243.04+temp_2m))))) %>%
+  select(lat, lon, datetime, downward_shortwave,downward_longwave, temp_2m,
+         relhum, wind_spd, precip, surface_press)
 
-colnames(era5met) <- colnames(met)
+colnames(era5met) <- c("lat", "lon", colnames(met)[-8])
+
+
+# arrow::write_parquet(era5met, sink = "data/era5_adk_1992-2012.parquet")
